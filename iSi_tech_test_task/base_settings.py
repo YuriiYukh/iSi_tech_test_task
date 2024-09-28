62372f4cc8d5591a86fb0c3e249d1e3503c97544
yuhim01 <yuhim01@gmail.com>
2024-09-28 12:47:01 +0300

    Add customized admin

--- iSi_tech_test_task/base_settings.py
+++ iSi_tech_test_task/base_settings.py
@@ -18,7 +18,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+ALLOWED_HOSTS = f"127.0.0.1 localhost {os.environ.get('DJANGO_ALLOWED_HOSTS')}".split(
+    " ")
 
 
 # Application definition
@@ -30,7 +31,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'djangorestframework',
+    'rest_framework',
 
     'message_threads',
 ]
--- message_threads/admin.py
+++ message_threads/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from message_threads.models import Thread, Message
 
-# Register your models here.
+
+class ThreadAdmin(admin.ModelAdmin):
+    '''
+    Custom admin for Thread model
+    '''
+    list_display = ('id', 'participants_list', 'created', 'updated')
+    search_fields = ('participants__username')
+    list_filter = ('created', 'updated')
+
+    def participants_list(self, obj: Thread):
+        '''
+        Display participants in comma-separated format
+        '''
+        return ', '.join(user.username for user in obj.participants.all())
+
+
+class MessageAdmin(admin.ModelAdmin):
+    '''
+    Custom admin for Message model
+    '''
+    list_display = ('id', 'sender', 'text_preview',
+                    'thread', 'created', 'is_read')
+    search_fields = ('sender__username', 'text')
+    list_filter = ('created', 'is_read')
+
+    def text_preview(self, obj: Message):
+        '''
+        Method to show the preview text of the message
+        '''
+        text = obj.text
+        return text[:50] + '...' if len(obj.text) > 50 else text
+
+
+admin.site.register(Thread)
+admin.site.register(Message)
